a9ebb71aa84dd14915fe07d9953f0cfa1499879a
peterpozo <peteranderson.pozo@gmail.com>
2022-10-24 17:33:32 +0200

    background-color sq change

--- js/app.js
+++ js/app.js
@@ -3,13 +3,13 @@ const eleSquares = document.querySelector('.squares');
 for (i=1; i<=100; i++) {
     if (i % 15 == 0){
     //   console.log('FizzBuzz');
-      eleSquares.innerHTML += `<div class="sq">${'FizzBuzz'}</div>`;
+      eleSquares.innerHTML += `<div class="sq fizzbuzz">${'FizzBuzz'}</div>`;
     } else if (i%3 == 0) {
     //   console.log('Fizz');
-      eleSquares.innerHTML += `<div class="sq">${'Fizz'}</div>`;
+      eleSquares.innerHTML += `<div class="sq fizz">${'Fizz'}</div>`;
     } else if(i%5 == 0) {
     //   console.log('Buzz');
-      eleSquares.innerHTML += `<div class="sq">${'Buzz'}</div>`;
+      eleSquares.innerHTML += `<div class="sq buzz">${'Buzz'}</div>`;
     } else {
         eleSquares.innerHTML += `<div class="sq">${i}</div>`;
     }
